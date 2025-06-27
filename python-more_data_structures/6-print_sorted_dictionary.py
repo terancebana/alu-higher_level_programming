ff7b0901b0d5fd104053cc05669bb2e403447e18
terancebana <cyzterance06@gmail.com>
2025-06-27 18:54:16 +0200

    Solved the remaining questions

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    if a_dictionary:
+        sorted_keys = sorted(a_dictionary.keys())
+        for key in sorted_keys:
+            print(f"{key}: {a_dictionary[key]}")
+    return
