4aa1d6b38f6698789bfce5dbbf67d42cff4ea3b9
terancebana <cyzterance06@gmail.com>
2025-06-27 10:38:44 +0200

    Fixed the output on question 6

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome"
-str2 = "School!"
-full_string = str1 + " to Holberton " + str2
-print(full_string)
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
