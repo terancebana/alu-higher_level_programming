5c6315e22664348f6e12f6684e019e86ab17fe35
terancebana <cyzterance06@gmail.com>
2025-07-09 14:26:48 +0000

    Fixed minor errors

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             count += 1
-        except IndexError:
-            break
+        except:
+            continue
     print()
     return (count)
